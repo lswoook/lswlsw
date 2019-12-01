7a0ce3f6f747abd209198ceb6438b010efca399d
lswoook <58388425+lswoook@users.noreply.github.com>
2019-12-02 00:12:16 +0900

    Add files via upload

---
+++ AD_Remove.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+using AutoItX3Lib;
+using OpenQA.Selenium.Firefox;
+using System.Windows;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using OpenQA.Selenium.Edge;
+using System.IO;
+
+namespace Bandposting2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+            string str, str1, str2, str3, before, after, chat;
+
+            int rCnt = 1, cCnt = 1, idc; // 열 갯수
+            int cCnt = 1; // 행 갯수
+            string[] IdList = new string[1000];
+            string[] LbF = new string[1000];
+            string[] LaF = new string[1000];
+            string[] Lct = new string[1000];
+            AutoItX3 autoit = new AutoItX3();
+            Random rand = new Random();
+            IWebDriver driver = new ChromeDriver(options);
+
+            Actions action = new Actions(driver);
+
+
+            //메인화면 접속
+            driver.Navigate().GoToUrl(URL);
+            autoit.Send("{ENTER}");
+            IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10.00));
+            IWait<IWebDriver> waitc = new WebDriverWait(driver, TimeSpan.FromSeconds(2.00));
+            IWait<IWebDriver> waitcc = new WebDriverWait(driver, TimeSpan.FromSeconds(1.00));
+            IWebElement query;
+
+            /*메인 로그인*/
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='header']/div/div/div/a[3]")));
+            query = driver.FindElement(By.XPath("//*[@id='header']/div/div/div/a[3]"));
+            query.Click();
+
+            /*이메일 로그인 선택*/
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='login_list']/li[2]/a")));
+            query = driver.FindElement(By.XPath("//*[@id='login_list']/li[2]/a"));
+            query.Click();
+
+            /* 아이디입력 */
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='input_email']")));
+            query = driver.FindElement(By.XPath("//*[@id='input_email']"));
+            query.SendKeys(IdList[n]);
+
+            Thread.Sleep(1000);
+
+            /* 비번입력 */
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='input_password']")));
+            query = driver.FindElement(By.XPath("//*[@id='input_password']"));
+            query.SendKeys(spw);
+
+            Thread.Sleep(1000);
+
+            /*로그인 버튼 누르기 >_< */
+            IWebElement submit = driver.FindElement(By.XPath("//*[@id='email_login_form']/button"));
+            submit.Click();
+
+            //캡챠
+            if (n == 0)
+            {
+                Console.WriteLine("캡챠 완료후 콘솔창 엔터하세요.");
+                Console.ReadKey();
+                Console.WriteLine("실행된 크롬창을 활성화 해주세요.");
+                Thread.Sleep(10000);
+            }
+            try
+            {
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='content']/div/section/header/div/div/button[1]")));
+                Console.WriteLine(IdList[n] + "로그인성공");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(IdList[n] + "로그인 실패 다시 시도합니다");
+                driver.Close();
+                continue;
+            }
+
+            //탭갯수
+            int ctab = 0;
+
+            /*밴드 정렬*/
+            try
+            {
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='content']/div/section/header/div/div/button[1]")));
+                IWebElement sort = driver.FindElement(By.XPath("//*[@id='content']/div/section/header/div/div/button[1]"));
+                sort.Click();
+                Thread.Sleep(1000);
+
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='content']/div/section/header/div/div/section[1]/div/div[2]/article/div/div[3]/div/div/a/span")));
+                IWebElement sort3 = driver.FindElement(By.XPath("//*[@id='content']/div/section/header/div/div/section[1]/div/div[2]/article/div/div[3]/div/div/a/span"));
+                sort3.Click();
+                Thread.Sleep(1000);
+                action.SendKeys(Keys.Down).Perform();
+                action.SendKeys(Keys.Down).Perform();
+                action.SendKeys(Keys.Enter).Perform();
+                Thread.Sleep(500);
+
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='content']/div/section/header/div/div/section[1]/div/div[2]/article/footer/div/button")));
+                IWebElement sortc = driver.FindElement(By.XPath("//*[@id='content']/div/section/header/div/div/section[1]/div/div[2]/article/footer/div/button"));
+                sortc.Click();
+                Thread.Sleep(1000);
+
+                autoit.Send("^t");
+                ctab++;
+                Thread.Sleep(1000);
+                driver.SwitchTo().Window(driver.WindowHandles[ctab]);
+                driver.Navigate().GoToUrl(URL);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("로그인 실패");
+                driver.Close();
+                continue;
+            }
+
+            int startb = frontb + 1;
+            Console.WriteLine(startb + "번째 블록에서 시작합니다");
+            int Bnum = 0;
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                /*밴드 갯수 세기*/
+                try
+                {
+                    wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='content']/div/section/div/ul/li")));
+                    Bnum = driver.FindElements(By.XPath("//*[@id='content']/div/section/div/ul/li")).Count - frontb - backb;
+                    Console.WriteLine("현재 밴드갯수:" + Bnum);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("모니터링 실패: " + IdList[n]);
+                }
+                Thread.Sleep(1000);
+
+                //밴드 순차적으로 들어가기
+                string bstr = "//*[@id='content']/div/section/div/ul/li[{0}]/div/a/div[1]/div/span";
+                string data = startb.ToString();
+                string address = string.Format(bstr, data);
+                try
+                {
+                    wait.Until(ExpectedConditions.ElementExists(By.XPath(address)));
+                    IWebElement band = driver.FindElement(By.XPath(address));
+                    band.Click();
+                }
+                catch (Exception)
+                {
+                    driver.SwitchTo().Window(driver.WindowHandles[ctab]).Close();
+                    ctab--;
+                    driver.SwitchTo().Window(driver.WindowHandles[ctab]);
+                    break;
+                }
+
+                //포스팅 내용중 광고성 메세지 포함여부 검사
+                try
+                {
+                    Thread.Sleep(2000);
+                    IWebElement textbox = driver.FindElement(By.XPath("//*[@id='content']/section/div[2]/div/button"));
+                    str context = textbox.getText; // 포스팅 내용을 context에 저장
+
+                    //이미 삭제 된 경우
+                    try
+                    {
+                        // Check the presence of alert
+                        IAlert alert = driver.SwitchTo().Alert();
+                        alert.Accept();
+                        driver.Navigate().GoToUrl(URL);
+                        if (startb == Bnum)
+                        {
+                            break;
+                        }
+                        continue;
+                    }
+                    catch (NoAlertPresentException) { }
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(1000);
+                    driver.SwitchTo().Window(driver.WindowHandles[ctab]);
+                    startb++;
+                    driver.Navigate().GoToUrl(URL);
+                    continue;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    if (context.Contains(keywords[i]))
+                        {
+
+                        //광고성 포스팅 삭제
+                        context.ClipPut(before);
+                        try
+                        {
+                            waitc.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='layerContainer']/div/section/div/div[2]/article/div/div/div[4]/div/button[2]")));
+                            IWebElement post2 = driver.FindElement(By.XPath("//*[@id='layerContainer']/div/section/div/div[2]/article/div/div/div[4]/div/button[2]"));
+                            action.Click(post2).Perform();
+                        }
+                        catch (Exception)
+                        {
+                        }
+                        Thread.Sleep(rand.Next(2000, 3000));
+                    }
+                }
+            }
+            //로그아웃
+            while (true)
+            {
+                try
+                {
+                    driver.SwitchTo().Window(driver.WindowHandles[0]);
+                    break;
+                }
+                catch (Exception)
+                {
+                }
+            }
+            try
+            {
+                // Check the presence of alert
+                Thread.Sleep(1000);
+                IAlert alert = driver.SwitchTo().Alert();
+                alert.Accept();
+            }
+            catch (NoAlertPresentException)
+            {
+            }
+        }
+    }
+}
---
+++ Band_Search.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+using OpenQA.Selenium.Firefox;
+using System.Windows;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using OpenQA.Selenium.Remote;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string id,pw,snkeywords, snmem, smaxf, sminf;
+            string[] kw = new string[1000];
+            int nmem , maxf , minf;
+
+            
+            object TypMissing = Type.Missing;
+            // Excel Application 정의
+            Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook _workbook = null;
+
+            // 파일이 존재 하면 열고, 없으면 새로 만듬.
+            if (File.Exists(@"d:\Test.xls"))
+            {
+                _workbook = ExcelApp.Workbooks.Open(@"c:\URl검색\url.xls", TypMissing, TypMissing, TypMissing, TypMissing,
+                    TypMissing, TypMissing,
+                    TypMissing, TypMissing, TypMissing, TypMissing, TypMissing, TypMissing, TypMissing, TypMissing);
+            }
+            else
+            {
+                // Add Work Book
+                _workbook = ExcelApp.Workbooks.Add(Type.Missing);
+                // Save Excel File
+                _workbook.SaveAs(@"c:\URL검색기\url.xls", Excel.XlFileFormat.xlWorkbookNormal, TypMissing, TypMissing,
+               TypMissing, TypMissing, Excel.XlSaveAsAccessMode.xlNoChange,
+               TypMissing, TypMissing, TypMissing, TypMissing, TypMissing);
+            }  
+
+            //키워드 읽기
+            string[] KWList = new string[1000];
+            StreamReader reader = new StreamReader(@"C:\밴드검색기\키워드.txt");
+            int k = 1;
+            while (reader.EndOfStream == false)
+            {
+                string line = reader.ReadLine();
+                KWList[k] = line;
+                Console.WriteLine(k + ":" + KWList[k]);
+                k++;
+            }
+
+            int Cline = k - 1;
+            Console.WriteLine(Cline + "개 키워드 검색 되었습니다.");
+            reader.Close();
+
+
+            string[] Black = new string[1000];
+            StreamReader reader2 = new StreamReader(@"C:\밴드검색기\블랙단어.txt");
+            int k1 = 1;
+
+            while (reader2.EndOfStream == false)
+            {
+                string line = reader2.ReadLine();
+                Black[k1] = line;
+                Console.WriteLine(k1 + ":" + Black[k1]);
+                k1++;
+            }
+
+            int CBlack = k1 - 1;
+            Console.WriteLine(CBlack + "개 블랙단어 검색 되었습니다.");
+            reader2.Close();
+
+            IWebElement query;
+
+            Console.Write("아이디:");
+            id = Console.ReadLine();
+
+            Console.Write("비밀번호:");
+            pw = Console.ReadLine();
+
+            //IWebDriver driver = new FirefoxDriver();
+
+            ChromeOptions opts = new ChromeOptions();
+            opts.AddArgument("--incognito");
+            IWebDriver driver = new ChromeDriver(opts); //<-Add your path
+
+            driver.Navigate().GoToUrl("http://band.us/#!/");
+            Console.WriteLine("Trying to access " + "http://band.us/#!/");
+            Actions action = new Actions(driver);
+            IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5.00));
+            IWait<IWebDriver> wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(1.00));
+
+            /*메인 로그인*/
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='header']/div/div/div/a[3]")));
+            IWebElement ML = driver.FindElement(By.XPath("//*[@id='header']/div/div/div/a[3]"));
+            ML.Click();
+
+            /*이메일 로그인 선택*/
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='login_list']/li[2]/a")));
+            IWebElement EB = driver.FindElement(By.XPath("//*[@id='login_list']/li[2]/a"));
+            EB.Click();
+
+            /* 아이디입력 */
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='input_email']")));
+            IWebElement ID = driver.FindElement(By.XPath("//*[@id='input_email']"));
+            ID.SendKeys(id);
+
+            /* 비번입력 */
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='input_password']")));
+            IWebElement PW = driver.FindElement(By.XPath("//*[@id='input_password']"));
+            PW.SendKeys(pw);
+
+            /*로그인 버튼 누르기*/
+            IWebElement submit = driver.FindElement(By.XPath("//*[@id='email_login_form']/button"));
+            submit.Click();
+
+
+            for (int cox = 1; cox <= Cline; cox++) // Cline = 키워드 갯수
+            {
+                int resultnum = 0;
+                //검색창 클릭 후 키워드 검색
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='input_search_view25']")));
+                query = driver.FindElement(By.XPath("//*[@id='input_search_view25']"));
+                action.Click(query).Perform();
+                action.SendKeys(KWList[cox]).Perform();
+                action.SendKeys(Keys.Enter).Perform();
+
+                //검색 결과를 기다린다            
+                string Tnum;
+                int num;
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='content']/div/div[2]/section/div/ul/li[2]")));
+
+                Tnum = driver.FindElement(By.XPath("//*[@id='content']/div/div[2]/section/div/h1/span")).Text;
+
+                num = Convert.ToInt32(Tnum);
+
+                Console.WriteLine(num + "개 검색 되었습니다.");
+                int rnum = 0;
+
+                for (int i = 1; i <= rnum; i++)
+                {
+                    //밴드로 이동
+                    string bstr = "//*[@id='content']/div/div[2]/section/div/ul/li[{0}]";
+                    string data = i.ToString();
+                    string address = string.Format(bstr, data);
+
+                    IWebElement bio = driver.FindElement(By.XPath(address));
+                    wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(address)));
+                    action.MoveToElement(bio).Perform();
+
+                    //밴드 멤버수 수집
+                    int mnum;
+                    string smnum;
+                    bstr = "//*[@id='content']/div/div[2]/section/div/ul/li[{0}]/div/a/div[2]/p[2]/span[1]/strong";
+                    address = string.Format(bstr, data);
+                    smnum = driver.FindElement(By.XPath(address)).Text.Replace(",", "");
+                    mnum = Convert.ToInt32(smnum);
+                    int blackflag = 0;
+
+                    //밴드명 수집
+                    string name;
+                    bstr = "//*[@id='content']/div/div[2]/section/div/ul/li[{0}]/div/a/div[2]/strong";
+                    address = string.Format(bstr, data);
+                    name = driver.FindElement(By.XPath(address)).Text;
+
+                    for (int kq = 1; kq <= CBlack; kq++)
+                    {
+                        if (name.Contains(Black[kq]))
+                        {
+                            blackflag = 1;
+                            break;
+                        }
+                    }
+
+                    if (blackflag == 0)
+                    {
+                        if (maxf == 0)
+                        {
+                            if (mnum >= minf)
+                            {
+                                //밴드주소, 밴드명 수집
+                                string link;
+                                bstr = "//*[@id='content']/div/div[2]/section/div/ul/li[{0}]/div/a";
+                                address = string.Format(bstr, data);
+
+                                link = driver.FindElement(By.XPath(address)).GetAttribute("href");
+                                using (StreamWriter sw = new StreamWriter(@"C:\밴드검색기\result.txt", true))
+                                {
+                                    sw.Write(KWList[cox] + ";");
+                                    sw.Write(link + ";");
+                                    sw.Write(mnum + ";");
+                                    sw.WriteLine(name + ";");
+                                    resultnum++;
+                                }
+                                /*
+                                Sheet.Cells[i, 1] = link;
+                                Sheet.Cells[i, 2] = mnum;
+                                Sheet.Cells[i, 3] = name;
+                                //Sheet.Rows.AutoFit();
+                                Sheet.Columns.AutoFit(); // 글자 크기에 맞게 셀 크기를 자동으로 조절.
+                                                         //Range_.Rows.AutoFit();
+                                                         //Range_.Columns.AutoFit();   
+                                _workbook.Save();
+                                */
+                            }
+                        }
+                        else if (minf == 0)
+                        {
+                            if (mnum <= maxf)
+                            {
+                                //밴드주소, 밴드명 수집
+                                string link;
+                                bstr = "//*[@id='content']/div/div[2]/section/div/ul/li[{0}]/div/a";
+                                address = string.Format(bstr, data);
+                                link = driver.FindElement(By.XPath(address)).GetAttribute("href");
+                                using (StreamWriter sw = new StreamWriter(@"C:\밴드검색기\result.txt", true))
+                                {
+                                    sw.Write(KWList[cox] + ";");
+                                    sw.Write(link + ";");
+                                    sw.Write(mnum + ";");
+                                    sw.WriteLine(name + ";");
+                                    resultnum++;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (minf <= mnum && mnum <= maxf)
+                            {
+                                //밴드주소, 밴드명 수집
+                                string link;
+                                bstr = "//*[@id='content']/div/div[2]/section/div/ul/li[{0}]/div/a";
+                                address = string.Format(bstr, data);
+                                link = driver.FindElement(By.XPath(address)).GetAttribute("href");
+                                using (StreamWriter sw = new StreamWriter(@"C:\밴드검색기\result.txt", true))
+                                {
+                                    sw.Write(KWList[cox] + ";");
+                                    sw.Write(link + ";");
+                                    sw.Write(mnum + ";");
+                                    sw.WriteLine(name + ";");
+                                    resultnum++;
+                                }
+                            }
+                        }
+                    }
+                }
+                /*
+                var LastRow = Sheet.UsedRange.Rows.Count;
+                LastRow = LastRow + Sheet.UsedRange.Row;
+                int ee = 0;
+
+                for (ee = 1; ee <= LastRow+1; ee++)
+                {
+                    if (ExcelApp.WorksheetFunction.CountA(Sheet.Rows[ee]) == 0)
+                        (Sheet.Rows[ee] as Microsoft.Office.Interop.Excel.Range).Delete();
+                }
+                _workbook.Save();
+                */
+                driver.Navigate().GoToUrl("http://band.us/#!/");
+                using (StreamWriter sw = new StreamWriter(@"C:\밴드검색기\추출갯수.txt", true))
+                {
+                    sw.WriteLine(";" + KWList[cox] + ";" + resultnum + ";");
+                }
+
+            }
+
+            /*
+            //작업완료후 엑셀 끄기
+            ExcelApp.Workbooks.Close(); // 별효과 없음.
+            ExcelApp.Quit();            // 별효과 없음.
+            // 강제로 Excel Porcess종료하기.
+            Process[] ExCel = Process.GetProcessesByName("EXCEL");
+            if (ExCel.Count() != 0)
+            {
+                ExCel[0].Kill();
+            }
+            */
+            Console.WriteLine("작업완료");
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
